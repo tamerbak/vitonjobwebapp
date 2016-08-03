d215e0367e39fbba63c0c5eaece0781329253387
thomas <t.guillot@vitonjob.com>
2016-08-03 18:56:39 +0200

    supp projet

--- app/app/public/javascripts/script.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by thomas on 02/08/2016.
- */
-$( document ).ready(function() {
-
-});
--- app/app/routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' });
-});
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = router;
-//# sourceMappingURL=index.js.map
