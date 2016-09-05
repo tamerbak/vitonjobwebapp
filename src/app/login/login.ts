2598c1c31fcda93a6b2e64e8283b4e2d85fc8345
amalrochd <amalrochd@gmail.com>
2016-09-05 23:38:25 +0200

    First commit

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,10 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
+import {LoadListService} from "../providers/load-list.service";
 
 @Component({
   directives: [
-    ROUTER_DIRECTIVES,
+    ROUTER_DIRECTIVES
   ],
   selector: '[login]',
   host: {
@@ -11,7 +12,40 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
   },
   template: require('./login.html'),
   encapsulation: ViewEncapsulation.None,
-  styles: [require('./login.scss')]
+  styles: [require('./login.scss')],
+  providers: [LoadListService]
 })
 export class LoginPage {
+	index: number;
+	phone: number;
+	email: string; 
+	password1: string;
+	password2: string;
+	pays = [];
+	
+	isIndexValid =true;
+	isPhoneNumValid = true;
+	showEmailField: boolean;
+	emailExist = false;
+	isRecruteur : boolean = false;
+	
+	libelleButton: string;
+	
+	constructor(private loadListService: LoadListService) {
+		
+	}
+	
+	ngOnInit(): void {
+		this.index = 33;
+		this.libelleButton = "Se connecter";
+		//load countries list
+		this.loadListService.loadCountries('employer').then((data) => {
+			this.pays = data.data;
+		});
+		
+	}
+	
+	authenticate(){
+		console.log("yataaaaa");
+	}
 }
