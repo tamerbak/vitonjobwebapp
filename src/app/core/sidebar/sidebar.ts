cbac639b30cfb12b0bd559c0631784faa395b18d
amalrochd <amalrochd@gmail.com>
2016-09-23 10:03:11 +0100

    fixes #76

--- src/app/core/sidebar/sidebar.ts
+++ src/app/core/sidebar/sidebar.ts
@@ -7,71 +7,72 @@ import {SharedService} from "../../providers/shared.service";
 declare var jQuery: any;
 
 @Component({
-  selector: '[sidebar]',
-  directives: [
-    ROUTER_DIRECTIVES,
-    SlimScroll
-  ],
-  template: require('./sidebar.html')
+	selector: '[sidebar]',
+	directives: [
+		ROUTER_DIRECTIVES,
+		SlimScroll
+	],
+	template: require('./sidebar.html')
 })
 
 export class Sidebar implements OnInit {
-  $el: any;
-  config: any;
-  router: Router;
-  location: Location;
-
-  currentUser:any;
-
-  constructor(config: ConfigService, el: ElementRef, router: Router, location: Location,private sharedService:SharedService) {
-    this.currentUser = this.sharedService.getCurrentUser();
-    this.$el = jQuery(el.nativeElement);
-    this.config = config.getConfig();
-    this.router = router;
-    this.location = location;
-  }
-
-  initSidebarScroll(): void {
-    let $sidebarContent = this.$el.find('.js-sidebar-content');
-    if (this.$el.find('.slimScrollDiv').length !== 0) {
-      $sidebarContent.slimscroll({
-        destroy: true
-      });
-    }
-    $sidebarContent.slimscroll({
-      height: window.innerHeight,
-      size: '4px'
-    });
-  }
-
-  changeActiveNavigationItem(location): void {
-    let $newActiveLink = this.$el.find('a[href="#' + location.path() + '"]');
-
-    // collapse .collapse only if new and old active links belong to different .collapse
-    if (!$newActiveLink.is('.active > .collapse > li > a')) {
-      this.$el.find('.active .active').closest('.collapse').collapse('hide');
-    }
-    this.$el.find('.sidebar-nav .active').removeClass('active');
-
-    $newActiveLink.closest('li').addClass('active')
-      .parents('li').addClass('active');
-
-    // uncollapse parent
-    $newActiveLink.closest('.collapse').addClass('in').siblings('a[data-toggle=collapse]').removeClass('collapsed');
-  }
-
-  ngAfterViewInit(): void {
-    this.changeActiveNavigationItem(this.location);
-  }
-
-  ngOnInit(): void {
-    jQuery(window).on('sn:resize', this.initSidebarScroll.bind(this));
-    this.initSidebarScroll();
-
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.changeActiveNavigationItem(this.location);
-      }
-    });
-  }
+	$el: any;
+	config: any;
+	router: Router;
+	location: Location;
+	
+	currentUser:any;
+	
+	constructor(config: ConfigService, el: ElementRef, router: Router, location: Location,private sharedService:SharedService) {
+		this.currentUser = this.sharedService.getCurrentUser();
+		this.$el = jQuery(el.nativeElement);
+		this.config = config.getConfig();
+		this.router = router;
+		this.location = location;
+	}
+	
+	initSidebarScroll(): void {
+		let $sidebarContent = this.$el.find('.js-sidebar-content');
+		if (this.$el.find('.slimScrollDiv').length !== 0) {
+			$sidebarContent.slimscroll({
+				destroy: true
+			});
+		}
+		$sidebarContent.slimscroll({
+			height: window.innerHeight,
+			size: '4px'
+		});
+	}
+	
+	changeActiveNavigationItem(location): void {
+		this.currentUser = this.sharedService.getCurrentUser();
+		let $newActiveLink = this.$el.find('a[href="#' + location.path() + '"]');
+		
+		// collapse .collapse only if new and old active links belong to different .collapse
+		if (!$newActiveLink.is('.active > .collapse > li > a')) {
+			this.$el.find('.active .active').closest('.collapse').collapse('hide');
+		}
+		this.$el.find('.sidebar-nav .active').removeClass('active');
+		
+		$newActiveLink.closest('li').addClass('active')
+		.parents('li').addClass('active');
+		
+		// uncollapse parent
+		$newActiveLink.closest('.collapse').addClass('in').siblings('a[data-toggle=collapse]').removeClass('collapsed');
+	}
+	
+	ngAfterViewInit(): void {
+		this.changeActiveNavigationItem(this.location);
+	}
+	
+	ngOnInit(): void {
+		jQuery(window).on('sn:resize', this.initSidebarScroll.bind(this));
+		this.initSidebarScroll();
+		
+		this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				this.changeActiveNavigationItem(this.location);
+			}
+		});
+	}
 }
