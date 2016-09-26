3160fa8e357b771299d3ccb0eb543c31f09b760d
adaoudi <Amine.Daoudi.7@gmail.com>
2016-09-26 10:10:54 +0100

    Adjust indents

--- src/app/core/notifications/notifications.ts
+++ src/app/core/notifications/notifications.ts
@@ -34,31 +34,31 @@ export class Notifications implements OnInit {
   }
 
   launchSearch(offer, noRedirect) {
-      if (!offer){
-          return;
-      }
+    if (!offer){
+      return;
+    }
 
-      let searchFields = {
-          class : 'com.vitonjob.callouts.recherche.SearchQuery',
-          job : offer.jobData.job,
-          metier : '',
-          lieu : '',
-          nom : '',
-          entreprise : '',
-          date : '',
-          table : this.projectTarget == 'jobyer'?'user_offre_entreprise':'user_offre_jobyer',
-          idOffre :'0'
-      };
-      this.searchService.criteriaSearch(searchFields, this.projectTarget).then(data => {
-          this.sharedService.setLastResult(data);
-          if(!noRedirect){
-            if(this.router.url === '/app/search/results'){
-              window.location.reload()
-            }else{
-              this.router.navigate(['app/search/results']);
-            }
-          }
-      });
+    let searchFields = {
+      class : 'com.vitonjob.callouts.recherche.SearchQuery',
+      job : offer.jobData.job,
+      metier : '',
+      lieu : '',
+      nom : '',
+      entreprise : '',
+      date : '',
+      table : this.projectTarget == 'jobyer'?'user_offre_entreprise':'user_offre_jobyer',
+      idOffre :'0'
+    };
+    this.searchService.criteriaSearch(searchFields, this.projectTarget).then(data => {
+      this.sharedService.setLastResult(data);
+      if(!noRedirect){
+        if(this.router.url === '/app/search/results'){
+          window.location.reload()
+        }else{
+          this.router.navigate(['app/search/results']);
+        }
+      }
+    });
   }
 
   moveNotificationsDropdown(): void {
