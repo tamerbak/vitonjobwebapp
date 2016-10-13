8fe02a71281df9ac60d7f18cdebd9b58f5147c06
amalrochd <amalrochd@gmail.com>
2016-10-13 18:04:32 +0100

    bug fix: offer notification should be shown before the profile modal

--- src/app/components/recruit-button/recruit-button.ts
+++ src/app/components/recruit-button/recruit-button.ts
@@ -32,42 +32,39 @@ export class RecruitButton {
   recruitJobyer() {
     this.currentUser = this.sharedService.getCurrentUser();
     if (this.currentUser && this.currentUser.employer) {
-      let userData = this.currentUser;
-      let currentEmployer = this.currentUser.employer;
-
-      //verification of employer informations
-      let redirectToCivility = (currentEmployer && currentEmployer.entreprises[0]) ?
-      (this.isEmpty(userData.titre)) ||
-      (this.isEmpty(userData.prenom)) ||
-      (this.isEmpty(userData.nom)) ||
-      (this.isEmpty(currentEmployer.entreprises[0].nom)) ||
-      (this.isEmpty(currentEmployer.entreprises[0].siret)) ||
-      (this.isEmpty(currentEmployer.entreprises[0].naf)) ||
-      (currentEmployer.entreprises[0].conventionCollective.id == 0) ||
-      (currentEmployer.entreprises[0].siegeAdress.id == 0) ||
-      (currentEmployer.entreprises[0].workAdress.id == 0) : true;
-
-      let isDataValid = !redirectToCivility;
-
-      if (isDataValid) {
-        let o = this.sharedService.getCurrentOffer();
-        //show the contract notification
-        if (o) {
+      let o = this.sharedService.getCurrentOffer();
+      //if there is no selected offer
+      if(!o || o == null){
+        this.obj = "offer";
+      }else{
+        //verify if employer profile is filled
+        let userData = this.currentUser;
+        let currentEmployer = this.currentUser.employer;
+        //verification of employer informations
+        let redirectToCivility = (currentEmployer && currentEmployer.entreprises[0]) ?
+        (this.isEmpty(userData.titre)) ||
+        (this.isEmpty(userData.prenom)) ||
+        (this.isEmpty(userData.nom)) ||
+        (this.isEmpty(currentEmployer.entreprises[0].nom)) ||
+        (this.isEmpty(currentEmployer.entreprises[0].siret)) ||
+        (this.isEmpty(currentEmployer.entreprises[0].naf)) ||
+        (currentEmployer.entreprises[0].conventionCollective.id == 0) ||
+        (currentEmployer.entreprises[0].siegeAdress.id == 0) ||
+        (currentEmployer.entreprises[0].workAdress.id == 0) : true;
+        let isDataValid = !redirectToCivility;
+        if (isDataValid) {
+          //show the contract notification
           this.obj = "contract";
-        } else {
-          //show the selection offer notif
-          this.obj = "offer";
+        }else{
+          //show profil notif
+          this.obj = "profile";
         }
-      } else {
-        //show profil notif
-        this.obj = "profile";
       }
       this.openModal(this.obj);
     }
     else {
       return;
     }
-    //this.onRecruite.emit(this.obj);
   }
 
   openModal(obj){
