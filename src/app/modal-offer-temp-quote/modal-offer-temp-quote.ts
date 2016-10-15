1baae4fc206c58e686ef696564923120b8258611
kguezou <kelvin.guezou@gmail.com>
2016-10-15 19:50:36 +0200

    Code review

--- src/app/modal-offer-temp-quote/modal-offer-temp-quote.ts
+++ src/app/modal-offer-temp-quote/modal-offer-temp-quote.ts
@@ -1,35 +1,16 @@
-import {Component, NgZone, Input} from "@angular/core";
-import {ROUTER_DIRECTIVES, Router} from "@angular/router";
-import {SharedService} from "../../providers/shared.service";
-import {OffersService} from "../../providers/offer.service";
-import {FinanceService} from '../../providers/finance.service'
-import {DomSanitizationService} from '@angular/platform-browser';
+import {Component} from "@angular/core";
+import {ROUTER_DIRECTIVES} from "@angular/router";
 
 declare var jQuery, require, Messenger: any;
 
 @Component({
   selector: 'modal-offer-temp-quote',
   directives: [ROUTER_DIRECTIVES],
-  providers: [OffersService, FinanceService],
   template: require('./modal-offer-temp-quote.html'),
   styles: [require('./modal-offer-temp-quote.scss')]
 })
 export class ModalOfferTempQuote {
 
-  @Input() params: any;
-
-  currentUser: any = null;
-  projectTarget: any;
-  processing: boolean = false;
-  quote: any;
-
-
-  constructor(private sharedService: SharedService,
-              private offersService: OffersService,
-              private financeService: FinanceService,
-              private zone: NgZone,
-              private sanitizer: DomSanitizationService,
-              private router: Router) {
-  }
+  constructor() {}
 
 }
