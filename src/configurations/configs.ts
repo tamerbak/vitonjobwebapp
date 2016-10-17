dc7c05fae1c5ca8cd08265af9b447ddbc586d8b6
amalrochd <amalrochd@gmail.com>
2016-10-17 18:32:33 +0100

    bug fix: correcting the url in configs class for docusign

--- src/configurations/configs.ts
+++ src/configurations/configs.ts
@@ -128,7 +128,9 @@ export class Configs {
 
   public static calloutURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
   public static sqlURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/sql';
-  public static yousignURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/callout';
+  //public static yousignURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/callout';
+  public static yousignURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
+
   public static smsURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoisms';
   public static emailURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoimail';
   public static fssURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/fssjs';
