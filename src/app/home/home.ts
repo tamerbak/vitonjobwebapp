cebf9c6f4dff6c1245042435d574a9f12d4106a9
jakjoud <jakjoud@gmail.com>
2016-10-25 09:01:13 +0200

    Loading offers and new users

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,6 +3,7 @@ import {ROUTER_DIRECTIVES, Router} from "@angular/router";
 import {AlertComponent} from "ng2-bootstrap/components/alert";
 import {SearchService} from "../../providers/search-service";
 import {SharedService} from "../../providers/shared.service";
+import {HomeService} from "../../providers/home.service";
 import {Configs} from "../../configurations/configs";
 import {ModalWelcome} from "../modal-welcome/modal-welcome";
 import {ModalProfile} from "../modal-profile/modal-profile";
@@ -16,7 +17,7 @@ declare var Messenger:any;
 	selector: 'home',
 	template: require('./home.html'),
 	directives: [ROUTER_DIRECTIVES, AlertComponent, ModalWelcome, ModalProfile,ModalUpdatePassword],
-	providers: [SearchService],
+	providers: [SearchService, HomeService],
 	styles: [require('./home.scss')],
 	encapsulation: ViewEncapsulation.None
 })
@@ -27,12 +28,26 @@ export class Home {
 	scQuery: string;
 	alerts: Array<Object>;
 	hideLoader: boolean = true;
-  config: any;
-  isTablet: boolean = false;
-
+    config: any;
+    isTablet: boolean = false;
+    /*
+     *  HOME SCREEN LISTS
+     */
+    recentOffers: any = [];
+    upcomingOffers: any = [];
+    recentUsers: any = [];
+    previousRecentOffers: any = [];
+    previousUpcomingOffers: any = [];
+    previousRecentUsers: any = [];
+    nextRecentOffers: any = [];
+    nextUpcomingOffers: any = [];
+    nextRecentUsers: any = [];
+    homeServiceData: any = [];
+    maxLines: number = 5;
 
   constructor(private router: Router,
               private searchService: SearchService,
+              private homeService: HomeService,
               private sharedService: SharedService) {
   }
 
@@ -68,6 +83,10 @@ export class Home {
       }
     } else {
       this.projectTarget = this.sharedService.getProjectTarget();
+      this.homeService.loadHomeData((this.projectTarget)).then(data=> {
+        this.homeServiceData = data;
+        this.initHomeList();
+      });
     }
 
     this.config = Configs.setConfigs(this.projectTarget);
@@ -90,8 +109,49 @@ export class Home {
 
     this.sharedService.setCurrentOffer(null);
 
+
   }
 
+  initHomeList() {
+    if (!this.homeServiceData || this.homeServiceData.length == 0)
+      return;
+
+    //  Let us start with recent offers
+
+    let data = this.homeServiceData.recentOffers;
+    let max = data.length > this.maxLines ? this.maxLines : data.length;
+    for (let i = 0; i < max; i++) {
+      this.recentOffers.push(data[i]);
+    }
+
+    for (let i = this.maxLines; i < data.length; i++) {
+      this.nextRecentOffers.push(data[i]);
+    }
+
+    //  Now we deal with upcoming offers
+
+    data = this.homeServiceData.upcomingOffers;
+    max = data.length > this.maxLines ? this.maxLines : data.length;
+    for (let i = 0; i < max; i++) {
+      this.upcomingOffers.push(data[i]);
+    }
+
+    for (let i = this.maxLines; i < data.length; i++) {
+      this.nextUpcomingOffers.push(data[i]);
+    }
+
+    //  Finally new users
+
+    data = this.homeServiceData.users;
+    max = data.length > this.maxLines ? this.maxLines : data.length;
+    for (let i = 0; i < max; i++) {
+      this.recentUsers.push(data[i]);
+    }
+
+    for (let i = this.maxLines; i < data.length; i++) {
+      this.nextRecentUsers.push(data[i]);
+    }
+  }
 
   ngOnDestroy(){
     jQuery('.content').css({"padding":"40px","padding-top":"60px"});
@@ -142,5 +202,152 @@ export class Home {
       return true;
     else
       return false;
+  }
+
+  previousUsers() {
+    if(this.previousRecentUsers.length == 0)
+      return;
+    this.nextRecentUsers = [];
+    for (let i = 0; i < this.recentUsers.length; i++)
+      this.nextRecentUsers.push(this.recentUsers[i]);
+
+    this.recentUsers = [];
+    for (let i = 0; i < this.previousRecentUsers.length; i++) {
+      this.recentUsers.push(this.previousRecentUsers[i]);
+    }
+
+    this.previousRecentUsers = [];
+    let offset = this.homeServiceData.query.startIndex - this.homeServiceData.query.resultCapacity;
+
+    if (offset <= 0) {
+      offset = 0;
+      this.homeServiceData.query.startIndex = offset;
+      return;
+    }
+
+    this.homeServiceData.query.startIndex = offset;
+    this.homeService.loadMore(this.projectTarget, this.homeServiceData.query.startIndex, this.homeServiceData.query.startIndexOffers).then((data: any)=> {
+      let newData = data.users;
+      let max = newData.length > this.maxLines ? this.maxLines : newData.length;
+      for (let i = 0; i < max; i++) {
+        this.previousRecentUsers.push(newData[i]);
+      }
+    });
+
+  }
+
+  nextUsers() {
+    if(this.nextRecentUsers.length == 0)
+      return;
+    this.previousRecentUsers = [];
+    for (let i = 0; i < this.recentUsers.length; i++)
+      this.previousRecentUsers.push(this.recentUsers[i]);
+
+    this.recentUsers = [];
+    for (let i = 0; i < this.nextRecentUsers.length; i++) {
+      this.recentUsers.push(this.nextRecentUsers[i]);
+    }
+
+    this.nextRecentUsers = [];
+    let offset = this.homeServiceData.query.startIndex + this.homeServiceData.query.resultCapacity;
+    this.homeServiceData.query.startIndex = offset;
+    this.homeService.loadMore(this.projectTarget, this.homeServiceData.query.startIndex, this.homeServiceData.query.startIndexOffers).then((data: any)=> {
+      let newData = data.users;
+      let max = newData.length > this.maxLines ? this.maxLines : newData.length;
+      for (let i = 0; i < max; i++) {
+        this.nextRecentUsers.push(newData[i]);
+      }
+    });
+
+  }
+
+  nextOffers() {
+    if(this.nextRecentOffers.length == 0)
+      return;
+
+    this.previousRecentOffers = [];
+    for (let i = 0; i < this.recentOffers.length; i++)
+      this.previousRecentOffers.push(this.recentOffers[i]);
+
+    this.recentOffers = [];
+    for (let i = 0; i < this.nextRecentOffers.length; i++)
+      this.recentOffers.push(this.nextRecentOffers[i]);
+
+    this.previousUpcomingOffers = [];
+    for (let i = 0; i < this.upcomingOffers.length; i++)
+      this.previousUpcomingOffers.push(this.upcomingOffers[i]);
+
+    this.upcomingOffers = [];
+    for (let i = 0; i < this.nextUpcomingOffers.length; i++)
+      this.upcomingOffers.push(this.nextUpcomingOffers[i]);
+
+    this.nextRecentOffers = [];
+    this.nextUpcomingOffers = [];
+    let offset = this.homeServiceData.query.startIndexOffers + this.homeServiceData.query.resultCapacityOffers;
+    this.homeServiceData.query.startIndexOffers = offset;
+    this.homeService.loadMore(this.projectTarget, this.homeServiceData.query.startIndex, this.homeServiceData.query.startIndexOffers).then((data: any)=> {
+      debugger;
+      let newData = data.recentOffers;
+      let max = newData.length > this.maxLines ? this.maxLines : newData.length;
+      for (let i = 0; i < max; i++) {
+        this.nextRecentOffers.push(newData[i]);
+      }
+      newData = data.upcomingOffers;
+      max = newData.length > this.maxLines ? this.maxLines : newData.length;
+      for (let i = 0; i < max; i++) {
+        this.nextUpcomingOffers.push(newData[i]);
+      }
+    });
+  }
+
+  previousOffers() {
+    if(this.previousRecentOffers.length == 0)
+      return;
+    this.nextRecentOffers = [];
+    for (let i = 0; i < this.recentOffers.length; i++)
+      this.nextRecentOffers.push(this.recentOffers[i]);
+
+    this.recentOffers = [];
+    for (let i = 0; i < this.previousRecentOffers.length; i++)
+      this.recentOffers.push(this.previousRecentOffers[i]);
+
+    this.nextUpcomingOffers = [];
+    for (let i = 0; i < this.upcomingOffers.length; i++)
+      this.nextUpcomingOffers.push(this.upcomingOffers[i]);
+
+    this.upcomingOffers = [];
+    for (let i = 0; i < this.previousUpcomingOffers.length; i++)
+      this.upcomingOffers.push(this.previousUpcomingOffers[i]);
+
+    this.previousRecentOffers = [];
+    this.previousUpcomingOffers = [];
+    let offset = this.homeServiceData.query.startIndexOffers - this.homeServiceData.query.resultCapacityOffers;
+    if (offset <= 0) {
+      offset = 0;
+      this.homeServiceData.query.startIndexOffers = offset;
+      return;
+    }
+
+    this.homeServiceData.query.startIndexOffers = offset;
+    this.homeService.loadMore(this.projectTarget, this.homeServiceData.query.startIndex, this.homeServiceData.query.startIndexOffers).then((data: any)=> {
+      let newData = data.recentOffers;
+      let max = newData.length > this.maxLines ? this.maxLines : newData.length;
+      for (let i = 0; i < max; i++) {
+        this.previousRecentOffers.push(newData[i]);
+      }
+      newData = data.upcomingOffers;
+      max = newData.length > this.maxLines ? this.maxLines : newData.length;
+      for (let i = 0; i < max; i++) {
+        this.previousUpcomingOffers.push(newData[i]);
+      }
+    });
+  }
+
+  simplifyDate(time) {
+    let d = new Date(time);
+    let str = d.getDate() + "/";
+    str = str + (d.getMonth() + 1) + "/";
+    str = str + d.getFullYear();
+    return str;
   }
 }
