26b6a340e21218479a77d2bf904674e8a2e34d45
Thomas Guillot <guillotthomas035@gmail.com>
2016-11-25 14:47:25 +0100

    about page correction

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,15 +1,22 @@
 import {Component, ViewEncapsulation} from "@angular/core";
-import {ROUTER_DIRECTIVES, Router} from "@angular/router";
-import {InProgressPage} from "../in-progress/in-progress";
+import {ROUTER_DIRECTIVES} from "@angular/router";
+import {SharedService} from "../../providers/shared.service";
+
 declare var jQuery, require: any;
 @Component({
   selector: '[about]',
   template: require('./about.html'),
-  directives: [ROUTER_DIRECTIVES, InProgressPage],
+  directives: [ROUTER_DIRECTIVES],
   encapsulation: ViewEncapsulation.None,
   styles: [require('./about.scss')]
 })
 export class About {
-  constructor(private router: Router) {
+
+  currentUser: any;
+  projectTarget: string;
+
+  constructor(private sharedService: SharedService) {
+    this.currentUser = this.sharedService.getCurrentUser();
+      this.projectTarget = (this.currentUser.estRecruteur ? 'employer' : (this.currentUser.estEmployeur ? 'employer' : 'jobyer'));
   }
 }
