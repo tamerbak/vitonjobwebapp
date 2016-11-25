6f8ae96adc3cb01658f74d3bff127566469072a6
Thomas Guillot <guillotthomas035@gmail.com>
2016-11-25 16:01:31 +0100

    nouvelle corection supression de la ligne

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,6 +1,5 @@
 import {Component, ViewEncapsulation} from "@angular/core";
 import {ROUTER_DIRECTIVES} from "@angular/router";
-import {SharedService} from "../../providers/shared.service";
 
 declare var jQuery, require: any;
 @Component({
@@ -11,12 +10,6 @@ declare var jQuery, require: any;
   styles: [require('./about.scss')]
 })
 export class About {
-
-  currentUser: any;
-  projectTarget: string;
-
-  constructor(private sharedService: SharedService) {
-    this.currentUser = this.sharedService.getCurrentUser();
-      this.projectTarget = (this.currentUser.estRecruteur ? 'employer' : (this.currentUser.estEmployeur ? 'employer' : 'jobyer'));
+  constructor() {
   }
 }
