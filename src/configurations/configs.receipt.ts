e6da596157f453c7d55531efa026d6f3eebf0621
kguezou <kelvin.guezou@gmail.com>
2017-02-02 11:15:10 +0100

    CONFIG : Update callout link on receipt environment

--- src/configurations/configs.receipt.ts
+++ src/configurations/configs.receipt.ts
@@ -127,12 +127,18 @@ export class Configs {
   public static env: string = 'DEV';
   public static isOnline: boolean = true;
 
-  public static calloutURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/business';
-  public static sqlURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/sql';
-  public static yousignURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/business';
-  public static smsURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/envoisms';
-  public static emailURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/envoimail';
-  public static fssURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/fssjs';
+  // public static calloutURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/business';
+  // public static sqlURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/sql';
+  // public static yousignURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/business';
+  // public static smsURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/envoisms';
+  // public static emailURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/envoimail';
+  // public static fssURL: string = ' http://vojrecette.us-west-2.elasticbeanstalk.com/api/fssjs';
+  public static calloutURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
+  public static sqlURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/sql';
+  public static yousignURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
+  public static smsURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoisms';
+  public static emailURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoimail';
+  public static fssURL: string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/fssjs';
 
   public static googleMapApiKey: string = 'AIzaSyD6de5QuqKPECTwNSkmBfeRmiTb9147S_Y';
 
