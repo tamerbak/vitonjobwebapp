496c2999772faf1dd4c8f7d52c468b11233424cc
kguezou <kelvin.guezou@gmail.com>
2017-02-10 19:19:59 +0100

    Restore adv content

--- src/providers/advert.service.ts
+++ src/providers/advert.service.ts
@@ -239,7 +239,7 @@ export class AdvertService {
       return "";
     let val = "";
     try {
-      val = decodeURIComponent(escape(atob(content)));
+      val = atob(content);
     } catch (exc) {
       val = content;
     }
